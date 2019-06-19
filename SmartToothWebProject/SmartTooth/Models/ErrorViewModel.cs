10894388e387a54207b308a09e24c5d0ec2fff92
HamletPoghosian <phamik@mail.ru>
2019-06-19 12:26:33 +0400

    add new Asp.Net MVC Core project

---
+++ SmartToothWebProject/SmartTooth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartTooth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

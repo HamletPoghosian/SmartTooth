f9092fbd6fa86c130ab9bfd04acd0af6fe398e3a
HamletPoghosian <phamik@mail.ru>
2019-06-25 19:50:39 +0400

    add new design

--- SmartToothWebProject/SmartTooth/wwwroot/js/site.js
+++ SmartToothWebProject/SmartTooth/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script type="text/javascript">
+    $(document).ready(function(){
+        var boxWidth = $(".box").width();
+		$(".box").mouseenter(function(){
+        $(this).animate({
+            width: "400"
+        });
+    }).mouseleave(function(){
+        $(this).animate({
+            width: boxWidth
+        });
+    });
+});
+</script>
